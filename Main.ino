5735f3df46f95c57547fbbe03b6a24f32cd3fce2
OchoaEstaPillado <125396394+OchoaEstaPillado@users.noreply.github.com>
2023-02-14 20:05:27 +0100

    Create Main.ino

---
+++ Main.ino
@@ -0,0 +1,87 @@
+#include <Servo.h>
+
+Servo yaw;
+Servo artikulazioa1;
+Servo artikulazioa2;
+Servo pinza;
+
+int pos = 0;
+
+int repetizioak = 0;
+
+void setup() {
+  yaw.attach(8);
+  artikulazioa1.attach(9);
+  artikulazioa2.attach(10);
+  pinza.attach(11);
+  Serial.begin(9600);
+  Serial.println("Mugimendua hasita");
+  repeticiones = 0;
+  delay(7000);
+}
+
+void loop() {
+  while (repetizioak > 7) {
+    delay(5000);
+    
+    yawAurreraka();
+    aurreraInklinatu();
+    pinzatu();
+    atzeraInklinatu();
+    yawAtzera();
+    aurreraInklinatu();
+    pinzatu();
+    atzeraInklinatu();
+    
+    Serial.println(repetizioak);
+    repetizioak++;
+  }
+}
+
+void aurreraInklinatu() {
+  for (pos = 2; pos <= 70; pos += 1) {
+    artikulazioa1.write(pos);
+    artikulazioa2.write(pos);
+    delay(15);
+  }
+  delay(200);
+}
+
+void AtzeraInklinatu() {
+  for (pos = 70; pos >= 2; pos -= 1) {
+    artikulazioa1.write(pos);
+    artikulazioa2.write(pos);
+    delay(15);
+  }
+  delay(200);
+}
+
+void pinzatu() {
+  for (pos = 0; pos <= 30; pos += 1) {
+    pinza.write(pos);
+    delay(15);
+  }
+  delay(100);
+  
+  for (pos = 30; pos >= 0; pos -= 1) {
+    pinza.write(pos);
+    delay(15);
+  }
+  delay(200);
+}
+
+void yawAurreraka() {
+  for (pos = 2; pos <= 190; pos += 1) {
+    yaw.write(pos);
+    delay(15);
+  }
+  delay(200);
+}
+
+void yawAtzeraka() {
+  for (pos = 190; pos >= 2; pos -= 1) {
+    yaw.write(pos);
+    delay(15);
+  }
+  delay(200);
+}
